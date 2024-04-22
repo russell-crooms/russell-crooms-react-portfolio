949a61ef6facd6cda7a94d97ce478fe22e688556
Russell Crooms <russella.crooms@gmail.com>
2024-04-21 19:13:19 -0500

    Replaced the Russell logo to make it smaller. Also completed initial styling of the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Navbar } from './components/navbar/Navbar'
+import Navbar from './components/navbar/Navbar'
+
 
 const App = () => {
   return (
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import './Navbar.css';
+import Russell_logo_final from '../../assets/russell-logo-final.png';
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+        <img src={Russell_logo_final} alt="Russell" />
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Skills</li>
+            <li>Portfolio</li>
+            <li>Contact Me</li>
+        </ul>
+        <div className="nav-connect">
+            Connect With Me
+        </div>
+    </div>
   )
 }
+
+export default Navbar;
