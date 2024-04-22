57345fc2bf18e9ee805901e110c00b450da3db4e
Russell Crooms <russella.crooms@gmail.com>
2024-04-21 19:57:36 -0500

    Finished adding elements, and styling the hero section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import "./Hero.css";
+import russell_hero_image from "../../assets/russell-hero-image.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <img src={russell_hero_image} alt="" />
+      <h1><span>I'm Russell Crooms,</span> Frontend Developer based in Texas, USA.</h1>
+      <p>
+        I am a Frontend Developer that specializes in using React, HTML, CSS,
+        JavaScript and other popular technologies to build beautiful, and
+        customer friendly apps.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          Connect With Me
+        </div>
+        <div className="hero-resume">
+          My Resume
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
