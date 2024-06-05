71e298a5673f2ca325b46c54d0a36facb307b760
Russell Crooms <russella.crooms@gmail.com>
2024-06-04 22:26:04 -0500

    Created the About component, and mounted this to the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
+import About from './components/about/About'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   )
 }
