50c4f6db819148e69fa14314d021b667218d7569
Russell Crooms <russella.crooms@gmail.com>
2024-06-22 19:25:42 -0500

    Mounted the Contact component to the App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import About from './components/about/About'
 import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <About />
       <Portfolio />
+      <Contact />
     </div>
   )
 }
