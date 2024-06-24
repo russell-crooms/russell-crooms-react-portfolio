099860e491cae4180e1d4008845e744190ff0de0
Russell Crooms <russella.crooms@gmail.com>
2024-06-23 23:53:40 -0500

    Making adjustments to the footer paragraph

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,22 @@
 import './Footer.css';
+import russell_logo from '../../assets/russell-logo-final.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+        <div className="footer-top">
+            <div className="footer-top-left">
+                <img src={russell_logo} alt="" />
+                <p>I am a Frontend Developer from USA with 10 years of experience in
+                companies like Microsoft, Tesla an Apple.</p>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
