6492a6901094b98a815a5dee3de7eb824acd80a3
Russell Crooms <russella.crooms@gmail.com>
2024-06-24 00:17:16 -0500

    Added the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,23 +1,21 @@
 import './Footer.css';
 import russell_logo from '../../assets/russell-logo-final.png';
+import user_icon from '../../assets/user_icon.svg';
 
 const Footer = () => {
   return (
     <div className="footer">
-        <div className="footer-top">
-            <div className="footer-top-left">
-                <img src={russell_logo} alt="" />
-                <p>I am a Frontend Developer from USA with 10 years of experience in
-                companies like Microsoft, Tesla an Apple.</p>
-            </div>
-            <div className="footer-top-right">
-                <div className="footer-email-input">
-                    <img src="" alt="" />
+            <hr />
+            <div className="footer-bottom">
+                <p className="footer-bottom-left"> @ 2024 Russell Crooms. All rights reserved</p>
+                <div className="footer-bottom-right">
+                    <p>Terms of Service</p>
+                    <p>Privacy Policy</p>
+                    <p>Connect With Me</p>
                 </div>
             </div>
         </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
